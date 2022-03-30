b866cf2c714004e70433eba01dc0d5f370cb4ea2
Mark Harris <mharris@technobear.com>
2022-03-30 16:57:31 +0200

    more cart ui

--- technobear/cart/Source/PluginEditor.cpp
+++ technobear/cart/Source/PluginEditor.cpp
@@ -9,9 +9,13 @@ using pcontrol_type = ssp::BarParamControl;
 using bcontrol_type = ssp::ParamButton;
 
 static constexpr unsigned MAX_LAYERS = PluginProcessor::MAX_LAYERS;
+static constexpr unsigned MAX_STEPS = PluginProcessor::MAX_STEPS;
 static constexpr unsigned MAX_VIEW = MAX_LAYERS * 2;
-static const unsigned MAX_PARAM_LINES = 4;
+static const unsigned MAX_X = 4, MAX_Y=4;
 
+inline float constrain(float v, float vMin, float vMax) {
+    return std::max<float>(vMin, std::min<float>(vMax, v));
+}
 
 PluginEditor::PluginEditor(PluginProcessor &p)
     : base_type(&p, MAX_VIEW),
@@ -25,8 +29,8 @@ PluginEditor::PluginEditor(PluginProcessor &p)
     for (unsigned layer = 0; layer < MAX_LAYERS; layer++) {
         auto &l = processor_.params_.layers_[layer % MAX_LAYERS];
         Colour clr = clrs_[layer % MAX_LAYERS];
-        for (unsigned i = 0; i < 16 / 4; i++) {
-            int pi = i * 4;
+        for (unsigned i = 0; i <  MAX_STEPS / MAX_Y; i++) {
+            int pi = i * MAX_X;
             addParamPage(
                 std::make_shared<pcontrol_type>(l->steps_[pi + 0]->cv, 0.25),
                 std::make_shared<pcontrol_type>(l->steps_[pi + 1]->cv, 0.25),
@@ -48,8 +52,8 @@ PluginEditor::PluginEditor(PluginProcessor &p)
                 encView
             );
         }
-        for (unsigned i = 0; i < 16 / 4; i++) {
-            int pi = i * 4;
+        for (unsigned i = 0; i <  MAX_STEPS / MAX_Y; i++) {
+            int pi = i * MAX_X;
             addButtonPage(
                 std::make_shared<bcontrol_type>(l->steps_[pi + 0]->glide, 24, clr),
                 std::make_shared<bcontrol_type>(l->steps_[pi + 1]->glide, 24, clr),
@@ -94,6 +98,106 @@ PluginEditor::PluginEditor(PluginProcessor &p)
 
 void PluginEditor::drawView(Graphics &g) {
     base_type::drawView(g);
+    int startX = 1000;
+    int y = 30;
+    int fh = 18;
+    int lsz = 75;;
+
+    int szx = fh * 4;
+    int szy = szx;
+    int sp = fh;
+
+    int x = startX;
+
+    g.setFont(Font(Font::getDefaultMonospacedFontName(), fh, Font::plain));
+    g.setColour(Colours::red);
+
+    x += sp + (szx * 2.5f);
+    g.drawText("Outputs", x, y, lsz, fh, Justification::left);
+
+    x = startX;
+    y += sp;
+
+
+    // draw header
+    g.drawText("Inputs", x, y, lsz, fh, Justification::left);
+    x += lsz + sp;
+    for (unsigned xi = 0; xi < MAX_X; xi++) {
+        if (processor_.isOutputEnabled(xi * 2))  {
+            g.setColour(Colours::red);
+        } else {
+            g.setColour(Colours::grey);
+        }
+        char chout[2] = "A";
+        chout[0] = 'A' + xi;
+        g.drawText(String(chout), x, y, szx, fh, Justification::centred);
+        x += szx;
+    }
+
+
+
+    static float values[MAX_X][MAX_Y];
+    static bool init= false;
+    if(!init) {
+        init=true;
+        Random random;
+        for (unsigned yi = 0; yi < MAX_Y; yi++) {
+            for (unsigned xi = 0; xi < MAX_X; xi++) {
+                values[xi][yi] = (random.nextFloat() * 2.0f) - 1.0f;
+            }
+        }
+    }
+
+    y += szy;
+    // draw each input line
+    for (unsigned yi = 0; yi < MAX_Y; yi++) {
+        x = startX;
+
+        if (processor_.isInputEnabled(yi * 2) ) {
+            g.setColour(Colours::red);
+        } else {
+            g.setColour(Colours::grey);
+        }
+        g.drawText(String(yi + 1), x, (y - (fh / 2)), lsz, fh, Justification::left);
+        x += lsz + sp;
+
+
+        for (unsigned xi = 0; xi < MAX_X; xi++) {
+//            float vca = processor_.getVCA(yi, xi) + processor_.getVCACV(yi, xi);
+            float vca = values[xi][yi];
+            vca = constrain(vca / 1.2f, -1.0f, 1.0f);
+
+            if (vca == 0.0f) {
+                unsigned d = 5;
+                unsigned o = ((szy - d) / 2);
+                unsigned cy = y - (szy / 2) + o;
+                unsigned cx = x + o;
+                g.setColour(Colours::white);
+                g.fillEllipse(cx, cy, d, d);
+                // g.drawEllipse(cx, cy, d, d, 1);
+            } else {
+                unsigned d = szy;
+                if (vca > 0.0f) {
+                    d = vca * szy;
+                    g.setColour(Colours::green);
+
+                } else {
+                    d = vca * -szy;
+                    g.setColour(Colours::red);
+                }
+                unsigned o = ((szy - d) / 2);
+                unsigned cy = y - (szy / 2) + o;
+                unsigned cx = x + o;
+                g.fillEllipse(cx, cy, d, d);
+            }
+            x += szx;
+
+            // String v = "-";
+            // if (vca <= -0.01f || vca >= 0.01f) v = String::formatted("%4.2f", vca);
+            // g.drawText(v, x + lx + xi * (szx + sp), y , sp + szx, fh, Justification::left);
+        }
+        y += szy;
+    }
 }
 
 void PluginEditor::resized() {
@@ -107,11 +211,11 @@ void PluginEditor::onLeftShiftButton(bool v) {
                 switch (cvButtonMode_) {
                     case B_GATEACCESS :
                         cvButtonMode_ = B_GLIDE;
-                        setButtonPage(buttonPage_ + MAX_PARAM_LINES);
+                        setButtonPage(buttonPage_ + MAX_Y);
                         break;
                     case B_GLIDE :
                         cvButtonMode_ = B_GATEACCESS;
-                        setButtonPage(buttonPage_ - MAX_PARAM_LINES);
+                        setButtonPage(buttonPage_ - MAX_Y);
                         break;
                     default:
                         break;
@@ -138,7 +242,7 @@ void PluginEditor::onUpButton(bool v) {
     if (!v) {
         chgParamPage(-1);
         unsigned nextP = buttonPage_;
-        unsigned minP = encoderMode_ == E_CV && cvButtonMode_ == B_GLIDE ? MAX_PARAM_LINES : 0;
+        unsigned minP = encoderMode_ == E_CV && cvButtonMode_ == B_GLIDE ? MAX_Y : 0;
         if (nextP > minP) nextP--;
         if (nextP != buttonPage_) setButtonPage(nextP);
     }
@@ -150,7 +254,7 @@ void PluginEditor::onDownButton(bool v) {
         chgParamPage(1);
         unsigned nextP = buttonPage_;
         unsigned maxP = encoderMode_ == E_CV && cvButtonMode_ == B_GATEACCESS ?
-                        MAX_PARAM_LINES : (views_[view_].buttonPages_.size());
+                        MAX_Y : (views_[view_].buttonPages_.size());
         if (nextP < (maxP - 1)) nextP++;
         if (nextP != buttonPage_) setButtonPage(nextP);
     }
