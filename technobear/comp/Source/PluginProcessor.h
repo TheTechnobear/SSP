a7b1d7233e930cb6260bf3e1e22d521677d5ec99
TheTechnobear <mharris@technobear.com>
2022-04-04 00:32:03 +0200

    comp: add colour

--- technobear/comp/Source/PluginProcessor.h
+++ technobear/comp/Source/PluginProcessor.h
@@ -69,6 +69,8 @@ public:
         return props;
     }
 
+    static Colour getIconColour() { return juce::Colours::green;}
+
 protected:
     juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
 private:
