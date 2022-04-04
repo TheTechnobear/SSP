3a0aa10be82ae5274ed1f5a08d4c343e5f742895
TheTechnobear <mharris@technobear.com>
2022-04-04 15:53:03 +0200

    move to generic sspapi.h

--- technobear/cart/Source/PluginProcessor.h
+++ technobear/cart/Source/PluginProcessor.h
@@ -151,13 +151,9 @@ public:
         return props;
     }
 
-    static Colour getIconColour() { return juce::Colours::lightblue;}
-
     void getActiveData(unsigned &xp, unsigned &yp, unsigned &cp,
                        float &xCv, float &yCv, float &cCv,
                        bool &xGate, bool &yGate, bool &cGate) const;
-
-
 protected:
     juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
 
