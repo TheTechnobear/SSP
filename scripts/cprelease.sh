a0d63f4a345eed216f27164831ae7641dc6c84f8
TheTechnobear <mharris@technobear.com>
2022-08-28 12:13:15 +0200

    prep release for dlyd/ldrf/omod

--- scripts/cprelease.sh
+++
@@ -1,3 +0,0 @@
-cp ./technobear/README.txt ./releases
-cp ./build/technobear/*/*/Release/VST3/*.vst3/Contents/armv7l-linux/*.so ./releases/plugins
-/opt/homebrew/bin/arm-linux-gnueabihf-strip --strip-unneeded ./releases/plugins/*
