a06d9422ca88fd306708386f7a4e9cc88350bd4c
TheTechnobear <mharris@technobear.com>
2024-06-21 15:57:22 +0200

    xmx move to /media/BOOT

--- scripts/copybuild.xmx.sh
+++ scripts/copybuild.xmx.sh
@@ -1,1 +1,1 @@
-scp  -O ~/projects/SSP.private/build.xmx/technobear/*/*/VST3/*.vst3/Contents/*/*.so root@192.168.0.160:/media/BOOT/Trax/plugins
+scp  -O ~/projects/SSP.private/build.xmx/technobear/*/*/VST3/*.vst3/Contents/*/*.so root@192.168.0.160:/media/BOOT/plugins
