dc120af74ddfbd79255752aa0e263d7db0031cc5
TheTechnobear <mharris@technobear.com>
2025-03-28 16:34:21 +0100

    update to current juce 8.0.6, prep for beta release

--- scripts/copybuild.xmx.sh
+++ scripts/copybuild.xmx.sh
@@ -1,1 +1,1 @@
-scp  -O ~/projects/SSP.private/build.xmx/technobear/*/*/VST3/*.vst3/Contents/*/*.so root@192.168.0.160:/media/BOOT/plugins
+scp  -O ~/projects/SSP.private/build.xmx/technobear/*/*/Release/VST3/*.vst3/Contents/*/*.so root@192.168.0.160:/media/BOOT/plugins
--- scripts/release.xmx.sh
+++ scripts/release.xmx.sh
@@ -1,8 +1,10 @@
 cp ./technobear/README.txt ./releases/xmx
 mkdir -p ./releases/xmx/plugins
 cp ~/projects/SSP.private/build.xmx/technobear/*/*/Release/VST3/*.vst3/Contents/*/*.so ./releases/xmx/plugins
-#/opt/homebrew/bin/arm-linux-gnueabihf-strip --strip-unneeded ./releases/xmx/plugins/*
+/opt/homebrew/bin/aarch64-elf-strip --strip-unneeded ./releases/xmx/plugins/*
 cd ~/projects/SSP.private/releases/xmx 
+# for trax host only
+mv plugins/trax.so .
 rm ../tb_plugins_xmx.zip
 zip -r ../tb_plugins_xmx.zip .
 cd ~/projects/SSP.private
